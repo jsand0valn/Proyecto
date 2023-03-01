74ad6702e0442f4e38ed4bfe0e99fa9cfe354d2e
jsand0valn <123041578+jsand0valn@users.noreply.github.com>
2023-02-28 19:19:48 -0600

    Falta submenu

---
+++ src/proyectoprogramacion1/ProyectoProgramacion1.java
@@ -0,0 +1,21 @@
+package proyectoprogramacion1;
+
+import java.util.Scanner;
+
+public class ProyectoProgramacion1 {
+ 
+    public static void main(String[] args) {
+        MenuPrincipal menu=new MenuPrincipal();
+        menu.menu_en_pantalla();
+        
+    }
+
+   
+
+    
+    }
+
+    
+
+
+
